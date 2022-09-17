fb9b3ba3cc6e2ba668416a86f6da1aa11ece67c8
Park JaeHyung <explainpark101@gmail.com>
2022-09-17 16:12:12 +0900

    디스코드 이미지 변경 코드

---
+++ js/discordEmoji.js
@@ -0,0 +1,28 @@
+let emoticons = {
+    '100coin' : "https://cdn.discordapp.com/emojis/963984293485510676.webp" 
+    ,Bow : "https://cdn.discordapp.com/emojis/963983899992666162.webp" 
+    ,Eliminated : "https://cdn.discordapp.com/emojis/963983899791343638.webp" 
+    ,Find : "https://cdn.discordapp.com/emojis/963984189693255690.webp" 
+    ,VoteNo : "https://cdn.discordapp.com/emojis/1017700923763593276.webp" 
+    ,VoteYes : "https://cdn.discordapp.com/emojis/1017700849159512064.webp" 
+    ,aigo : "https://cdn.discordapp.com/emojis/965819299941986324.webp" 
+    ,aigoletter : "https://cdn.discordapp.com/emojis/965819299832954900.webp" 
+    ,hyangsojye : "https://cdn.discordapp.com/emojis/970136230685724712.webp" 
+    ,kingstamp2 : "https://cdn.discordapp.com/emojis/965956008935571456.webp" 
+    ,uprising2 : "https://cdn.discordapp.com/emojis/970136406896824340.webp" 
+    ,kingstamp1 : "https://cdn.discordapp.com/emojis/966246351925051432.gif" 
+    ,nice : "https://cdn.discordapp.com/emojis/966276781697007616.gif" 
+    ,uprising : "https://cdn.discordapp.com/emojis/970136299807866941.gif" 
+}
+
+let emote_names = ["100coin", "Bow", "Eliminated", "Find", "VoteNo", "VoteYes", "aigo", "aigoletter", "hyangsojye", "kingstamp2", "uprising2", "kingstamp1", "nice", "uprising"];
+
+let aritcle = document.querySelector("div#article-content");
+let comments = document.querySelectorAll("div.reply-content");
+
+emote_names.forEach((name)=>{
+    aritcle.innerHTML = article.innerHTML.replace(`:${name}:`, `<img src="${emoticons[name]}">`);
+    comments.forEach((comment)=>{
+        comment.innerHTML = comment.innerHTML.replace(`:${name}:`, `<img src="${emoticons[name]}">`);
+    })
+});
